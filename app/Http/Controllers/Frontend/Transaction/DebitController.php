ccaefae0dd5e3f270a8ec9ad541590a584f0f097
utxo.one <111649294+utxo-one@users.noreply.github.com>
2022-09-24 13:17:54 -0400

    Deposits and Debits Endpoints (#6)

--- app/Http/Controllers/Frontend/Transaction/DebitController.php
+++ app/Http/Controllers/Frontend/Transaction/DebitController.php
@@ -9,7 +9,7 @@ use App\Services\TransactionService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
 
-class DepositController extends Controller
+class DebitController extends Controller
 {
     public function __construct(private TransactionService $transactionService)
     {
@@ -18,20 +18,8 @@ class DepositController extends Controller
     public function index(): JsonResponse
     {
         return response()->json(
-            auth()->user()->transactions()->where('type', TransactionType::CREDIT)->paginate(),
+            auth()->user()->transactions()->where('type', TransactionType::DEBIT)->paginate(),
             Response::HTTP_OK
         );
-    }
-
-    public function store(StoreDepositRequest $request)
-    {
-        $invoice = $this->transactionService->createInvoice($request->amount);
-
-        return response()->json($invoice->getData(), Response::HTTP_OK);
-    }
-
-    public function show()
-    {
-        return view('transaction.deposit.show');
     }
 }
