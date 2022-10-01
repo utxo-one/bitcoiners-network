5be990bd9a67314392c30ab400bbcd5389b464b4
utxo.one <github@utxo.one>
2022-10-01 16:21:43 -0400

    Classification Vote System + Endpoints

--- database/migrations/2022_09_30_011848_add_public_metrics_to_tweets_table.php
+++ database/migrations/2022_09_30_011848_add_public_metrics_to_tweets_table.php
@@ -14,10 +14,10 @@ return new class extends Migration
     public function up()
     {
         Schema::table('tweets', function (Blueprint $table) {
-            $table->integer('replies')->default(0);
-            $table->integer('retweets')->default(0);
-            $table->integer('likes')->default(0);
-            $table->integer('quotes')->default(0);
+            $table->integer('twitter_replies')->default(0);
+            $table->integer('twitter_retweets')->default(0);
+            $table->integer('twitter_likes')->default(0);
+            $table->integer('twitter_quotes')->default(0);
         });
     }
 
@@ -29,10 +29,10 @@ return new class extends Migration
     public function down()
     {
         Schema::table('tweets', function (Blueprint $table) {
-            $table->dropColumn('replies');
-            $table->dropColumn('retweets');
-            $table->dropColumn('likes');
-            $table->dropColumn('quotes');
+            $table->dropColumn('twitter_replies');
+            $table->dropColumn('twitter_retweets');
+            $table->dropColumn('twitter_likes');
+            $table->dropColumn('twitter_quotes');
         });
     }
 };
