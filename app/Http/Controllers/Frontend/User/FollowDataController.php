c2cc22e44f78cfba52d8fb57af71edab87c05408
utxo.one <github@utxo.one>
2022-10-16 12:51:12 -0400

    Cache Follow Data with Redis

--- app/Http/Controllers/Frontend/User/FollowDataController.php
+++ app/Http/Controllers/Frontend/User/FollowDataController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Redis;
 
 class FollowDataController extends Controller
 {
@@ -13,9 +14,33 @@ class FollowDataController extends Controller
     {
         $user = User::where('twitter_username', $username)->firstOrFail();
 
-        return response()->json([
+        $followDataCache = Redis::get("follow_data:{$user->twitter_id}");
+
+        if ($followDataCache) {
+            return response()->json(json_decode($followDataCache), Response::HTTP_OK);
+        }
+
+        $followData = [
             'following_data' => $user->following_data,
             'follower_data' => $user->follower_data,
-        ], Response::HTTP_OK);
+        ];
+
+        $cacheTime = 60;
+
+        if ($user->twitter_count_followers > 1000) {
+            $cacheTime = 600;
+        }
+
+        if ($user->twitter_count_followers > 5000) {
+            $cacheTime = 1800;
+        }
+
+        if ($user->twitter_count_followers > 10000) {
+            $cacheTime = 86400;
+        }
+
+        Redis::setex("follow_data:{$user->twitter_id}", $cacheTime, json_encode($followData));
+
+        return response()->json($followData, Response::HTTP_OK);
     }
 }
