3222959f2e98c11a2f397ff2ffd0d9082a4312f5
utxo.one <github@utxo.one>
2022-10-17 21:14:28 -0400

    Reclassification of Users

--- config/classifier.php
+++ config/classifier.php
@@ -2,12 +2,12 @@
 
 return [
     'shitcoinerKeywords' => [
-        'eth', 'ada', 'dot', 'link', 'ethereum', 'cardano', 'polkadot', 'chainlink', 'bnb', 'nft', 'hex', 'pulsechain', 'solana', 'crypto', 'doge', 'binance', 'dogecoin', 'xrp', 'dao', 'dai', 'polygon', 'matic', 'shiba','shib','tron', 'avalanche', 'avax', 'link', 'chainlink', 'defi', 'luna', 'lunatic', 'uniswap', 'litecoin', 'ltc', 'algorand', 'monero', 'crypto', 
-        'Eth', 'Ada', 'Dot', 'Link', 'Ethereum', 'Cardano', 'Polkadot', 'Chainlink', 'Bnb', 'Nft', 'Hex', 'Pulsechain', 'Solana', 'Crypto', 'Doge', 'Binance', 'Dogecoin', 'Xrp', 'Dao', 'Dai', 'Polygon', 'Matic', 'Shiba','Shib','Tron', 'Avalanche', 'Avax', 'Link', 'Chainlink', 'Defi', 'Luna', 'Lunatic', 'Uniswap', 'Litecoin', 'Ltc', 'Algorand', 'Monero', 'Crypto',
-        'ETH', 'ADA', 'DOT', 'LINK', 'ETHEREUM', 'CARDANO', 'POLKADOT', 'CHAINLINK', 'BNB', 'NFT', 'HEX', 'PULSECHAIN', 'SOLANA', 'CRYPTO', 'DOGE', 'BINANCE', 'DOGECOIN', 'XRP', 'DAO', 'DAI', 'POLYGON', 'MATIC', 'SHIBA','SHIB','TRON', 'AVALANCHE', 'AVAX', 'LINK', 'CHAINLINK', 'DEFI', 'LUNA', 'LUNATIC', 'UNISWAP', 'LITECOIN', 'LTC', 'ALGORAND', 'MONERO', 'CRYPTO', 
-        '#eth', '#ada', '#dot', '#link', '#ethereum', '#cardano', '#polkadot', '#chainlink', '#bnb', '#nft', '#hex', '#pulsechain', '#solana', '#crypto', '#doge', '#binance', '#dogecoin', '#xrp', '#dao', '#dai', '#polygon', '#matic', '#shiba','#shib','#tron', '#avalanche', '#avax', '#link', '#chainlink', '#defi', '#luna', '#lunatic', '#uniswap', '#litecoin', '#ltc', '#algorand', '#monero', '#crypto',
-        '#Eth', '#Ada', '#Dot', '#Link', '#Ethereum', '#Cardano', '#Polkadot', '#Chainlink', '#Bnb', '#Nft', '#Hex', '#Pulsechain', '#Solana', '#Crypto', '#Doge', '#Binance', '#Dogecoin', '#Xrp', '#Dao', '#Dai', '#Polygon', '#Matic', '#Shiba','#Shib','#Tron', '#Avalanche', '#Avax', '#Link', '#Chainlink', '#Defi', '#Luna', '#Lunatic', '#Uniswap', '#Litecoin', '#Ltc', '#Algorand', '#Monero', '#Crypto',
-        '#ETH', '#ADA', '#DOT', '#LINK', '#ETHEREUM', '#CARDANO', '#POLKADOT', '#CHAINLINK', '#BNB', '#NFT', '#HEX', '#PULSECHAIN', '#SOLANA', '#CRYPTO', '#DOGE', '#BINANCE', '#DOGECOIN', '#XRP', '#DAO', '#DAI', '#POLYGON', '#MATIC', '#SHIBA','#SHIB','#TRON', '#AVALANCHE', '#AVAX', '#LINK', '#CHAINLINK', '#DEFI', '#LUNA', '#LUNATIC', '#UNISWAP', '#LITECOIN', '#LTC', '#ALGORAND', '#MONERO', '#CRYPTO',
+        'eth', 'ada', 'dot', 'link', 'ethereum', 'cardano', 'polkadot', 'chainlink', 'bnb', 'nft', 'hex', 'pulsechain', 'solana', 'doge', 'binance', 'dogecoin', 'xrp', 'dao', 'dai', 'polygon', 'matic', 'shiba','shib','tron', 'avalanche', 'avax', 'link', 'chainlink', 'defi', 'luna', 'lunatic', 'uniswap', 'litecoin', 'ltc', 'algorand', 'monero', 'bsv', 'bch',
+        'Eth', 'Ada', 'Dot', 'Link', 'Ethereum', 'Cardano', 'Polkadot', 'Chainlink', 'Bnb', 'Nft', 'Hex', 'Pulsechain', 'Solana', 'Doge', 'Binance', 'Dogecoin', 'Xrp', 'Dao', 'Dai', 'Polygon', 'Matic', 'Shiba','Shib','Tron', 'Avalanche', 'Avax', 'Link', 'Chainlink', 'Defi', 'Luna', 'Lunatic', 'Uniswap', 'Litecoin', 'Ltc', 'Algorand', 'Monero', 'Bsv', 'Bch',
+        'ETH', 'ADA', 'DOT', 'LINK', 'ETHEREUM', 'CARDANO', 'POLKADOT', 'CHAINLINK', 'BNB', 'NFT', 'HEX', 'PULSECHAIN', 'SOLANA', 'DOGE', 'BINANCE', 'DOGECOIN', 'XRP', 'DAO', 'DAI', 'POLYGON', 'MATIC', 'SHIBA','SHIB','TRON', 'AVALANCHE', 'AVAX', 'LINK', 'CHAINLINK', 'DEFI', 'LUNA', 'LUNATIC', 'UNISWAP', 'LITECOIN', 'LTC', 'ALGORAND', 'MONERO', 'BSV', 'BCH',
+        '#eth', '#ada', '#dot', '#link', '#ethereum', '#cardano', '#polkadot', '#chainlink', '#bnb', '#nft', '#hex', '#pulsechain', '#solana', '#doge', '#binance', '#dogecoin', '#xrp', '#dao', '#dai', '#polygon', '#matic', '#shiba','#shib','#tron', '#avalanche', '#avax', '#link', '#chainlink', '#defi', '#luna', '#lunatic', '#uniswap', '#litecoin', '#ltc', '#algorand', '#monero', '#bsv', '#bch',
+        '#Eth', '#Ada', '#Dot', '#Link', '#Ethereum', '#Cardano', '#Polkadot', '#Chainlink', '#Bnb', '#Nft', '#Hex', '#Pulsechain', '#Solana', '#Doge', '#Binance', '#Dogecoin', '#Xrp', '#Dao', '#Dai', '#Polygon', '#Matic', '#Shiba','#Shib','#Tron', '#Avalanche', '#Avax', '#Link', '#Chainlink', '#Defi', '#Luna', '#Lunatic', '#Uniswap', '#Litecoin', '#Ltc', '#Algorand', '#Monero', '#Bsv', '#Bch',
+        '#ETH', '#ADA', '#DOT', '#LINK', '#ETHEREUM', '#CARDANO', '#POLKADOT', '#CHAINLINK', '#BNB', '#NFT', '#HEX', '#PULSECHAIN', '#SOLANA', '#DOGE', '#BINANCE', '#DOGECOIN', '#XRP', '#DAO', '#DAI', '#POLYGON', '#MATIC', '#SHIBA','#SHIB','#TRON', '#AVALANCHE', '#AVAX', '#LINK', '#CHAINLINK', '#DEFI', '#LUNA', '#LUNATIC', '#UNISWAP', '#LITECOIN', '#LTC', '#ALGORAND', '#MONERO', '#BSV', '#BCH',
     ],
 
     'shitcoinerNames' => [
@@ -15,10 +15,12 @@ return [
     ],
 
     'bitcoinerNames' => [
-        'bitcoin', 'btc', 'Bitcoin', 'BTC', '⚡️', 'cRyPto', 'CrYpTo', '∞/21M', '₿'
+        'bitcoin', 'btc', 'Bitcoin', 'BTC', '⚡️', '∞/21M', '₿'
     ],
 
     'bitcoinerKeywords' => [
-        'bitcoin', '∞/21M', 'btc', '#bitcoin', '#btc', '#BITCOIN', 'Bitcoin', 'BTC', '#Bitcoin', '#Btc', 'sats', '⚡️', 'satoshi', 'satoshi nakamoto', 'Satoshi', 'Satoshi Nakamoto', 'SATS', 'SATOSHI', 'SATOSHI NAKAMOTO', 'sat', 'shitcoin', 'shitcoins'
+        'bitcoin', '∞/21M', 'btc', '#bitcoin', '#btc', '#BITCOIN', 'Bitcoin', 'BTC', '#Bitcoin', '#Btc', 'sats', '⚡️', 'satoshi', 'satoshi nakamoto', 'Satoshi', 'Satoshi Nakamoto', 'SATS', 'SATOSHI', 'SATOSHI NAKAMOTO', 'sat', 'shitcoin', 'shitcoins', 
+        'bitcoin.', 'btc.', '#bitcoin.', '#btc.', '#BITCOIN.', 'Bitcoin.', 'BTC.', '#Bitcoin.', '#Btc.',
+        'bitcoin,', 'btc,', '#bitcoin,', '#btc,', '#BITCOIN,', 'Bitcoin,', 'BTC,', '#Bitcoin,', '#Btc,',
     ],
 ];
